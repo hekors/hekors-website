4abac5e6c1258e7838916b16a72b10c2857088fa
Yash Sehgal <yashsehgal.work@gmail.com>
2022-01-04 15:15:49 +0000

    [UI Page]: MLH LocalHackDay-Build page added

---
+++ src/pages/Hackathons/MLHLocalHackDayBuild2022.js
@@ -0,0 +1,36 @@
+
+export default function MLHLocalHackDayBuild2022() {
+    return (
+        <div className="MLHLocalHackDayBuild2022-page">
+            <div className="mlh-localhackday-build-2022-hero-section-wrapper content-center"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    width: '90%',
+                    marginRight: 'auto',
+                    marginLeft: 'auto',
+                    marginTop: '3.4rem',
+                    paddingTop: '2.4rem',
+                    paddingBottom: '2.4rem'
+                }}
+            >
+                <div className="hero-section-content-wrapper">
+                    <h1 className="hero-section-title" style={{ fontSize: '46px' }}>
+                        Happy New Year <span style={{ fontWeight: '500' }}>🥂</span> <br />
+                        <span style={{ color: 'var(--h-pink)' }}>MLH LocalHackDay: Build</span> <br />
+                        is here. 
+                    </h1>
+                    <p className="hero-section-description" style={{ fontSize: '18px', fontWeight: '600', marginTop: '0.4rem' }}>
+                        Hekors, It's time to ASSEMBLE!
+                    </p>
+                </div>
+                <img src="https://i2.wp.com/www.4ye.co.uk/wp-content/uploads/2014/09/avengers-gif_o_145258.gif?resize=500%2C268&ssl=1"
+                    alt="developer-image" 
+                    style={{ width: '540px', height: 'auto', borderRadius: '16px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)' }}
+                />
+            </div>  
+        </div>
+    )
+}
