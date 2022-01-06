9344d4d8cbfe7b2d3635ea0870853e180c4a47ad
Yash Sehgal <yashsehgal.work@gmail.com>
2022-01-06 09:25:38 +0000

    [UI Component #14]: Footer component links added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import Button from '../elements/Button';
 import './footer-style.css';
-import { FaGithub } from 'react-icons/fa'
+import { FaDiscord, FaExternalLinkAlt, FaGithub } from 'react-icons/fa'
 
 export default function Footer() {
     return (
@@ -14,9 +14,26 @@ export default function Footer() {
                 }}
             >
                 <h4>Made by HEKORS. for HEKORS 💜</h4>
-                <div className="footer-options-wrapper">
+                <div className="footer-options-wrapper"
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        gap: '0.4rem'
+                    }}
+                >
                 <Button onClick={() => window.open('https://discord.gg/cge6rB9RXm')}>
-                    <FaGithub /> Join Discord
+                    <FaDiscord /> Join Discord
+                </Button>
+                <Button type="plain" 
+                    onClick={() => window.open('https://github.com/hekors/hekors-website/issues')}>
+                    <FaGithub /> Found issues? Contribute
+                </Button>
+                <Button type="plain" 
+                    style={{ backgroundColor: 'black', color: 'white' }}
+                    onClick={() => window.open('https://mlh.io/seasons/2022/events')}
+                >
+                    Hackathons @MLH <FaExternalLinkAlt />
                 </Button>
                 </div>
             </div>
